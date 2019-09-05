3f74869a3cdac2ac7ad29daaebc1178bdb9d1391
ivelasq <ivelasq@gmail.com>
2019-09-04 23:48:45 -0700

    add frpm to mode

--- code/ca_analysis.R
+++ code/ca_analysis.R
@@ -21,6 +21,11 @@ ca_enroll <-
 ca_frpm <-
   read_excel(here::here("data", "raw", "frpm1819.xlsx"), sheet = 2)
 
+colnames(ca_frpm) <- as.character(unlist(ca_frpm[1,]))
+ca_frpm = ca_frpm[-1, ]
+
+ca_frpm <- ca_frpm %>% clean_names()
+
 # el file
 ca_el <-
   read_delim(here::here("data", "raw", "fileselsch.txt"), delim = "\t")
@@ -32,7 +37,8 @@ ca_el <-
 
 ca_enroll_district <-
   ca_enroll %>% 
-  mutate(ethnic_name = case_when(ethnic == 0 ~ "Not Reported",
+  mutate(district_code = str_sub(cds_code, 3, 7),
+         ethnic_name = case_when(ethnic == 0 ~ "Not Reported",
                                  ethnic == 1 ~ "American Indian or Alaska Native",
                                  ethnic == 2 ~ "Asian",
                                  ethnic == 3 ~ "Pacific Islander",
@@ -41,15 +47,41 @@ ca_enroll_district <-
                                  ethnic == 6 ~ "African American",
                                  ethnic == 7 ~ "White",
                                  ethnic == 9 ~ "Two or More Races")) %>% 
-  select(district, ethnic_name, enr_total) %>% 
-  group_by(district, ethnic_name) %>% 
+  select(district_code, ethnic_name, enr_total) %>% 
+  group_by(district_code, ethnic_name) %>% 
   summarise(enr_total = sum(enr_total, na.rm = T)) %>% 
   mutate(prop = enr_total/sum(enr_total)) %>% 
   select(-enr_total) %>% 
   spread(ethnic_name, prop, -1, fill = NA) %>% 
-  mutate(demographics = case_when(White < .5 ~ "Majority Non-White",
+  mutate(demo_bucket = case_when(White < .5 ~ "Majority Non-White",
                                   TRUE ~ "Majority White"))
   
+nrow(ca_enroll_district) # 1034 districts
+
+# We want to create a bucket for FRPM.
+
+ca_frpm_district <-
+  ca_frpm %>% 
+  select(district_code, enrollment_k_12, frpm_count_k_12) %>% # we need to calculate frpm percentage so that it is a weighted average
+  mutate_at(vars(enrollment_k_12, frpm_count_k_12), funs(as.numeric)) %>% 
+  group_by(district_code) %>% 
+  summarise_all(funs(sum(.))) %>% 
+  mutate(district_frpm_pct = frpm_count_k_12/enrollment_k_12,
+         frpm_bucket = case_when(district_frpm_pct > .59 ~ "More than 59% FRPM",
+                                 TRUE ~ "Less than 59% FRPM"))
+
+summary(ca_frpm_district$district_frpm_pct) # let's see what the data looks like
+# I used the median to decide the bucket
+sum(is.na(ca_frpm_district$frpm_bucket))
+nrow(ca_frpm_district)
+
+# join the datasets
+
+full_district_data <-
+  full_join(ca_enroll_district, ca_frpm_district, by = "district_code")
+
+sum(is.na(full_district_data$frpm_bucket))
+nrow(full_district_data)
 
 # RUN ANALYSIS ------------------------------------------------------------
 
@@ -61,6 +93,6 @@ grouper <- function(data, ...) {
 }
 
 modal <- 
-  ca_enroll_district %>% 
-  grouper(demographics)
+  full_district_data %>% 
+  grouper(demo_bucket, frpm_bucket)
 
